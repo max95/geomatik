9b648e8ef4512db40dfb5bb768dc11d9fac21ab6
Max95 <jeromebouchet@mbp-jerome.home>
2018-09-01 19:48:19 +0200

    maj du dataframe avec les TPS de parcours

--- calcul_bpe_sante.py
+++ calcul_bpe_sante.py
@@ -11,16 +11,17 @@ import configparser
 
 
 ##Chargement config postgres
-config = configparser.RawConfigParser() 
+config = configparser.RawConfigParser()
 config.read('./config')
 
 ##Lecture du fichier source
 my_data = pd.read_csv('./equip-serv-sante-com-2017.csv', encoding = "utf-8", sep=';')
 df = pd.DataFrame(my_data)
+#df = df.set_index("CODGEO")
 
 ##Creation d'un fichier echantillon
-df_sample = df.sample(frac=0.005, replace=True)
-df_final = df_sample['CODGEO'].copy
+df = df.sample(frac=0.005, replace=True)
+df_final = df.copy()
 
 #Connexion a la Base De Donnees
 dsn = "host={} port={} dbname={} user={} password={}".format(config.get('POSTGRES','host'), config.get('POSTGRES','port'), config.get('POSTGRES','db'), config.get('POSTGRES','user'), config.get('POSTGRES','pass'))
@@ -30,46 +31,47 @@ cursor = conn.cursor()
 ##Parcourir les colonnes à la recher des NB_
 for col in df.columns:
     col_name = df[col].name
-    
+
     if (col_name[0:3] == "NB_"): #Si unr colonne commene par NB_, on continu le traitement
             liste_equip = []    #Variable pour une de commune disposant de la ressource et RAZ
             liste_resultat = [] #Pour préparer le tableau final et RAZ
-            
+
             #Obtenir la liste des communes disposant de la ressource
-            df_equip = (df_sample.loc[df[col_name]>0,:])
-            
+            df_equip = (df.loc[df[col_name]>0,:])
+
 #            print(col_name)
-            
+
             #Creation d'une variable contenant les communes équipées de la ressource
             liste_equip = df_equip["CODGEO"].tolist()
 
-                    
             #####DEBUG
 #            print(liste_equip)
-            
-            with tqdm(total=len(df_sample)) as pbar:
+
+            with tqdm(total=len(df)) as pbar:
                     #Recherche de la commune la plus proche et récupération du tps de parcours
-                    for index, row in df_sample.iterrows():
+                    for index, row in df.iterrows():
                         ##DEBUG
                         #print(row['CODGEO'])
-                        
+
 #                        query_sql = "SELECT \"TPS\" from matrice_depcom WHERE \"DEPCOM_START\" = '" + row['CODGEO'] + "' AND \"DEPCOM_STOP\" IN %s order BY \"TPS\" limit 1" % repr(tuple(map(str,liste_equip)))
                         query_sql = "SELECT \"TPS\" from matrice_depcom WHERE \"DEPCOM_START\" = '{}' AND \"DEPCOM_STOP\" IN {} order BY \"TPS\" limit 1".format(row['CODGEO'], repr(tuple(map(str,liste_equip))))
 
-                            
+
                         try:
                                 cursor.execute(query_sql, liste_equip)
                                 TPS = cursor.fetchone()[0]
-
+                                print(TPS)
                         except:
                                 #print("erreur SQL")
                                 TPS = 'NULL'
+                        #Mise à jour du tableau
+                        df_final.loc[df_final['CODGEO'] == row['CODGEO'] , col_name] = TPS
 
-                        liste_resultat.append(TPS)
+                        #liste_resultat.append(TPS)
                         pbar.update(1)
+                        #df_final.to_csv(col_name)
 
                     ##Ajout d'une colonne audataframe final avec les resultat précédent
-                    df_final.loc[:, col_name] = liste_resultat
-                    print(df_final)
 
-
+df_final.to_csv('beta_bpe.csv')
+                    #print(df_final)
